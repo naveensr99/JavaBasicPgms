8bf612ff884652c727c883c91edaae0fc0b5a789
naveensr99 <120403070+naveensr99@users.noreply.github.com>
2025-05-24 08:59:10 +0530

    Update baseTest.java

--- src/test/java/baseTest.java
+++ src/test/java/baseTest.java
@@ -1,1 +1,15 @@
+import ecolab.koerber.entities.RandomValue;
+
+public class BaseTest extends BrowserFactory {
+
+  protected WrapperClass wrapperClass;
+  protected RandomValue randomValue = new RandomValue();
+
+ 
+  
+  public BaseTest() {
+    wrapperClass = new WrapperClass(driverManager);
+    DriverManager.setWrapperClass(wrapperClass);
+  }
+}
 
